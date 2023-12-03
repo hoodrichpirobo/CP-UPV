cc12868d3481133ccf511b388b18de095596d316
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2023-12-03 14:41:19 +0100

    Create Plantilla.java

---
+++ Plantilla.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Plantilla{
+    
+    static java.util.Scanner in;
+    
+    public static void casoDePrueba(){
+        
+    }
+    
+    public static void main(String[] args){
+        
+        in = new java.util.Scanner(System.in);
+        
+        int numCasos = in.nextInt();
+        for(int i = 0; i < numCasos; i++)
+            casoDePrueba();
+    }
+}
