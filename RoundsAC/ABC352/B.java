8e2e3230f8923a3d18014280f5971b958ead7805
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2024-05-04 15:53:08 +0200

    Create B.java

---
+++ RoundsAC/ABC352/B.java
@@ -0,0 +1,34 @@
+// ______   __  __   __  __    
+///\  ___\ /\ \/\ \ /\_\_\_\   
+//\ \ \____\ \ \_\ \\/_/\_\/_  
+// \ \_____\\ \_____\ /\_\/\_\ 
+//  \/_____/ \/_____/ \/_/\/_/
+
+import java.util.*;
+
+public class Main {
+    static Scanner in;
+    
+    public static void testCase(){
+        in = new Scanner(System.in);
+        String correct = in.nextLine();
+        String incorrect = in.nextLine();
+        int[] coincides = new int[correct.length()];
+
+        for(int i = 0; i < correct.length(); i++){
+            for(int j = i; j < incorrect.length(); j++){
+                if(correct.charAt(i)==incorrect.charAt(j)){
+                    coincides[i++] = j + 1;
+                }
+            }
+        }
+
+        for(int k = 0; k < coincides.length; k++){
+            System.out.print(coincides[k] + " ");
+        }
+    }
+    
+    public static void main(String[] args){
+        testCase();
+    }
+}
