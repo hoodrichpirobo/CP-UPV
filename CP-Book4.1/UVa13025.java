9a488fb9e52e017e380a76531f57f86b243d1857
CUX "INDIO" PRADA <74674131+hoodrichpirobo@users.noreply.github.com>
2024-08-07 03:20:11 +0200

    Create UVa13025.java

---
+++ CP-Book4.1/UVa13025.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static PrintWriter out;
+
+    public static void main(String[] args) throws Exception{
+        out = new PrintWriter(System.out);
+        out.printf("May 29, 2013 Wednesday\n");
+        out.close();
+    }
+}
