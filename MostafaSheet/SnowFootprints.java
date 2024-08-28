8682db426c4828f21ed83022c61e9911950a879d
CUX "INDIO" PRADA <74674131+hoodrichpirobo@users.noreply.github.com>
2024-08-28 17:16:42 +0200

    broke my streak man

--- MostafaSheet/SnowFootprints.java
+++ MostafaSheet/SnowFootprints.java
@@ -1,27 +1,36 @@
 import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out = new PrintWriter(System.out);
 
         int n = Integer.parseInt(in.readLine());
-        String road = in.readLine();
+        String line = in.readLine();
+        char[] blocks = line.toCharArray();
 
-        for (int i = 0; i < n; i++) {
-            if (road.charAt(i) != '.') {
-                if (road.charAt(i) == 'R') {
-                    while (road.charAt(i) == 'R') {
-                        i++;
-                    }
-                    out.println((i) + " " + (i + 1));
-                } else {
-                    out.println((i + 1) + " " + (i));
+        for(int i = 0; i < n; i++){
+            if(blocks[i] == '.') continue;
+
+            if(blocks[i] == 'L'){
+                out.println((line.lastIndexOf('L') + 1) + " " + i);
+                break;
+            }
+
+            if(blocks[i] == 'R'){
+                int r = line.lastIndexOf('R') + 1;
+                i++;
+
+                if(blocks[r] == 'L'){
+                    out.println(i + " " + r);
+                }else{
+                    out.println(i + " " + (r + 1));
                 }
+
                 break;
             }
         }
-
+        
         out.close();
     }
 }
