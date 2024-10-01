c8de017b6b29b84fd58fee1323c8ab37f0879c0b
CUX "INDIO" PRADA <74674131+hoodrichpirobo@users.noreply.github.com>
2024-10-01 10:06:08 +0200

    Create digits.java

---
+++ MostafaSheet/digits.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class digits {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        String x;
+        while (true) {
+            x = in.readLine().trim();                           //42                            5
+            if (x.equals("END")) {
+                break;
+            }
+            
+            int i = 1;                  // .n
+            while (true) {
+                String x_next = Integer.toString(x.length());   //2         1           1       1       1
+                if (x.equals(x_next)) { //x_i = x_i+1             42 != 2   2 != 1      1==1    5!=1    1==1
+                    break;
+                } else {
+                    x = x_next;                                 //2         1                   1
+                    i++;                                        //a.n       ab.n                a.n
+                }
+            }
+            out.println(i);                                     //3                                     2
+        }
+        out.close();
+    }
+}
+
