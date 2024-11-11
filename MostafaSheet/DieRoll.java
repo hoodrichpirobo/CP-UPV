9c696f64fb7fcbea726987c5562406c409e5840d
CUX PRADA <cuxelputisimoamo@gmail.com>
2024-11-11 20:28:38 +0100

    there we go using gcd

--- MostafaSheet/DieRoll.java
+++ MostafaSheet/DieRoll.java
@@ -2,6 +2,19 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
+    public static int gcd(int A, int B) {
+        int divisor = Math.max(A,B);
+
+        while(divisor >= 1){
+            if(A % divisor == 0 && B % divisor == 0){
+                return divisor;
+            }
+            divisor--;
+        }
+
+        return 0;
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out = new PrintWriter(System.out);
@@ -9,16 +22,10 @@ public class Main{
         StringTokenizer tokens = new StringTokenizer(in.readLine());
         int Y = Integer.parseInt(tokens.nextToken()),
             W = Integer.parseInt(tokens.nextToken()),
-            A = (6 - Math.max(Y, W)) + 1 , B = 6;
-        String ans = A + "/" + B;
-       
-        if((double) (A/A)/(B/A) == (double)A/B){
-            ans = (A/A) + "/" + (B/A);
-        }else if((double) (A/2)/(B/2) == (double) A/B){
-            ans = (A/2) + "/" + (B/2);
-        }
-        
-        out.println(ans);
+            A = (6 - Math.max(Y, W)) + 1 , B = 6,
+            gcd = gcd(A, B);
+
+        out.println(A/gcd + "/" + B/gcd);
 
         out.close();
     }
