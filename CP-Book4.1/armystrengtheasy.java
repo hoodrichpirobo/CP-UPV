fbc0d711bfca1ada4e7cc8500e00460453c4bb3a
CUX PRADA <cuxelputisimoamo@gmail.com>
2024-12-17 21:12:12 +0100

    new method

---
+++ CP-Book4.1/armystrengtheasy.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class armystrengtheasy{
+    static BufferedReader in;
+    static PrintWriter out;
+
+    public static void solve() throws Exception {
+        StringTokenizer tokens = new StringTokenizer(in.readLine());
+        int nG = Integer.parseInt(tokens.nextToken()),
+            nM = Integer.parseInt(tokens.nextToken()),
+            maxG = Integer.MIN_VALUE, maxM = Integer.MIN_VALUE;
+
+        tokens = new StringTokenizer(in.readLine());
+        for(int i = 0; i < nG; i++){
+            maxG = Math.max(Integer.parseInt(tokens.nextToken()), maxG);
+        }
+
+        tokens = new StringTokenizer(in.readLine());
+        for(int i = 0; i < nM; i++){
+            maxM = Math.max(Integer.parseInt(tokens.nextToken()), maxM);
+        }
+
+        if(maxM > maxG){
+            out.println("MechaGodzilla");
+        }else{
+            out.println("Godzilla");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+
+        int TC = Integer.parseInt(in.readLine());
+        while(TC-->0){
+            in.readLine();
+            solve();
+        }
+
+        out.close();
+    }
+}
